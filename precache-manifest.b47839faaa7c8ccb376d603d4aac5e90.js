c768d7b50b54c76f206c982b6af2b75613939e0a
Uwem Uke <slyart147@gmail.com>
2019-11-29 03:58:52 +0100

    Updates

---
+++ precache-manifest.b47839faaa7c8ccb376d603d4aac5e90.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c687acfbd8ffd9dc76cb44a988618a3",
+    "url": "/teamwork/index.html"
+  },
+  {
+    "revision": "cff8295567d3a19adca2",
+    "url": "/teamwork/static/css/2.b1aaf2b4.chunk.css"
+  },
+  {
+    "revision": "ed3310cb55879ff104c0",
+    "url": "/teamwork/static/css/main.bd7e11ec.chunk.css"
+  },
+  {
+    "revision": "cff8295567d3a19adca2",
+    "url": "/teamwork/static/js/2.26be4ca4.chunk.js"
+  },
+  {
+    "revision": "ed3310cb55879ff104c0",
+    "url": "/teamwork/static/js/main.02c42453.chunk.js"
+  },
+  {
+    "revision": "09dbad3e70f6164212dc",
+    "url": "/teamwork/static/js/runtime-main.8c9b6c70.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/teamwork/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/teamwork/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/teamwork/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/teamwork/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/teamwork/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- src/shared/baseUrl.js
+++
@@ -1,2 +0,0 @@
-//export const baseUrl = 'http://localhost:3000/';
-export const baseUrl = 'https://secure-castle-56382.herokuapp.com/';
