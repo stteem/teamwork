656891ec0bd4a4294b5f6feb08c53de1d7cb22c9
Uwem Uke <slyart147@gmail.com>
2020-02-04 23:00:48 +0100

    404

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Main/>
       </div>
